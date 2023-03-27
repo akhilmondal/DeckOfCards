fc28c00624452007e05520e3aca595c7cb9d9056
akhilmondal <akhilmandalfamily@gmail.com>
2023-03-27 21:44:05 +0530

    Added: Deck of Cards program to the master.

---
+++ src/com/deckofcards/DeckOfCards.java
@@ -0,0 +1,45 @@
+package com.deckofcards;
+
+import java.util.Arrays;
+import java.util.Random;
+public class DeckOfCards {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to deck of card problem!");
+        String []suits = {"Clubs", "Diamonds", "Hearts", "Spades"};
+        String []rank={"2","3","4","5","6","7","8","9","10","Jack","Queen","King","Ace"};
+        int deckOfCard = suits.length*rank.length;
+        System.out.println(deckOfCard);
+        String deck[] = new String[deckOfCard];
+        // initialize the deck of cards
+        for (int i = 0; i < rank.length; i++) {
+            for (int j = 0; j < suits.length; j++) {
+                deck[suits.length * i + j] = rank[i] + " of " + suits[j];
+            }
+        }
+        // shuffle cards
+        Random random=new Random();
+        for(int i=deckOfCard-1;i>0;i--){
+            int j = random.nextInt(i+1);
+            String temp = deck[i];
+            deck[i] = deck[j];
+            deck[j] = temp;
+        }
+        // Assign value to each player
+        String [][]players =new String[4][9];
+        int index=0;
+        for(int i=0;i<4;i++){
+            for(int j=0;j<9;j++){
+                players[i][j]=deck[index++];
+            }
+        }
+        // Print players
+        for (int i=0;i<4;i++){
+            System.out.println("player : "+i);
+            for(int j=0;j<9;j++){
+                System.out.println(players[i][j]);
+            }
+            System.out.println();
+        }
+    }
+}
